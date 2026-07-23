334b2bd21a8a4f2bd1222789ad59ad9013abc162
LMH_1 <703678297@qq.com>
2026-07-23 18:37:52 +0800

    ai检视修改

--- testcases/ubsvirt/Overcommitment/VM_Escape/Mem_Borrow/test_test_vm_mem_borrow_010.py
+++ testcases/ubsvirt/Overcommitment/VM_Escape/Mem_Borrow/test_test_vm_mem_borrow_010.py
@@ -14,16 +14,17 @@ class TestVmMemBorrow010(OpenStackBaseCase):
     EnvType:
         None
     CaseName:
-        验证不同节点上多虚拟机同时加压后内存借用成功  --8p
+        验证不同numa上多虚拟机同时加压后借用成功
     PreCondition:
-        P1、环境中存在4个及以上节点
+        P1、环境中存在2个及以上节点
         P2、OpenStack/RackManager功能正常无异常
-        P3、Node0、Node1上配置20G可用大页内存
-        P4、已完成不同节点内存规格4G虚拟机VM1、8G虚拟机VM2的创建
+        P3、Node0的numa0上配置4G可用大页，numa1上配置8G可用大页
+        P4、已完成不同numa内存规格4G虚拟机VM1和8G虚拟机VM2的创建
+        P5、node1配置8G可用大页
     TestStep:
         S1、登录VM1/VM2，对VM1/VM2加压，使得内存超过第二水位线92%，查看水位线告警、借用策略、借入借出点水位线告警变化情况
     ExpectedResult:
-        E1、两个节点存在水位线告警，预期借用总收益2G，借用账本借用量各1G，触发内存借用操作，内存借入点水位线会降低，内存借出点水线会上涨。
+        E1、两个numa存在水位线告警，预期借用总收益1G，借用账本借用量各1G，触发内存借用操作，内存借入点水位线会降低，内存借出点水线会上涨。
     Author:
         fq
     """
@@ -32,15 +33,18 @@ class TestVmMemBorrow010(OpenStackBaseCase):
         self.clear_server()
 
     def test_vm_mem_borrow_010(self, get_topo_path):
-
-        self.logStep("P4、已完成不同节点内存规格4G虚拟机VM1、8G虚拟机VM2的创建")
+        self.logStep(" P1、环境中存在2个及以上节点")
+        self.logStep(" P2、OpenStack/RackManager功能正常无异常")
+        self.logStep(" P4、 P3、Node0的numa0上配置4G可用大页，numa1上配置8G可用大页")
+        self.logStep(" P4、已完成不同numa内存规格4G虚拟机VM1和8G虚拟机VM2的创建")
+        self.logStep(" P4、node1配置8G可用大页")
         self.vms = self.prepare_topo(str(get_topo_path("test_test_vm_mem_borrow_010")))
 
-        self.logStep("S1、登录虚拟机对虚拟机进行加压至95%，借用1G内存")
+        self.logStep("S1、登录VM1/VM2，对VM1/VM2加压，使得内存超过第二水位线92%，查看水位线告警、借用策略、借入借出点水位线告警变化情况")
         for vm in self.vms:
             self.add_stress_to_vm(vm, 96)
 
         self.logStep(
-            "E1、.两个numa存在水位线告警，预期借用总收益1G，分别借用账本借用量1G，触发内存借用操作，水位线会降低，内存借出点水线会上涨。"
+            "E1、两个numa存在水位线告警，预期借用总收益1G，借用账本借用量各1G，触发内存借用操作，内存借入点水位线会降低，内存借出点水线会上涨。"
         )
         self.assertTrue(self.check_borrowed_numa_size("node1", 1000, 2048), "the borrowed size is not 2048M")
