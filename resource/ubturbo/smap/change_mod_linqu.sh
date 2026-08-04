491bb70e9366b86aed7f864f427ba7510c252e55
tanxin <120132945@qq.com>
2026-08-04 16:19:56 +0800

    smap修改

--- resource/ubturbo/smap/change_mod_linqu.sh
+++ resource/ubturbo/smap/change_mod_linqu.sh
@@ -15,7 +15,7 @@ function __prepare_dependencies()
     if [ ! -f "${SMAP_RESOURCE_HOME}/${vm_image_name}" ]
     then
       scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
-        root@controller:/opt/install/tmp/openstack/images/${vm_image_name} ${SMAP_RESOURCE_HOME}
+        root@controller:/opt/install/package/${vm_image_name} ${SMAP_RESOURCE_HOME}
     fi
     virsh list --name | xargs -i virsh destroy {}
     __cp_resource ${SMAP_RESOURCE_HOME}/${vm_image_name} ${SMAP_CASE_WORKSPACE}/vm/img
